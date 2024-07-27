95b6ea052bf28fa585c59ae69ee0251cc73858b1
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-07-27 16:35:47 -0400

    set up frontend a tad bit, going to be using shadcn, its customizable 100%, which is nice

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Button className="rounded-e-3xl ">eddie</Button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
