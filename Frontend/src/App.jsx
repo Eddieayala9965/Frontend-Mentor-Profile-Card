ac1b2ea1fc97f19b8928cd953cf53f44af331e98
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-01 16:25:10 -0400

    navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import SignUpForm from "./components/SignUpForm";
+import LoginForm from "./components/LoginForm";
 
 import "./index.css";
 
-const App = () => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Profile />
+          </Layout>
+        }
+      />
+    </Routes>
+    <Routes>
+      <Route
+        path="/signup"
+        element={
+          <Layout>
+            <SignUpForm />
+          </Layout>
+        }
+      />
+    </Routes>
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          <Layout>
+            <LoginForm />
+          </Layout>
+        }
+      />
+    </Routes>
+  </Router>
+);
 
 export default App;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>hello world</div>;
+  return (
+    <nav className=" container rounded-full h-10  bg-zinc-600 flex flex-col mt-3">
+      <div className=" my-auto mx-auto flex">
+        <ul>
+          <Link to="/" className="text-white p-4">
+            Home
+          </Link>
+          <Link to="/signup" className="text-white p-4">
+            Sign Up
+          </Link>
+          <Link to="/login" className="text-white p-4">
+            Login
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
